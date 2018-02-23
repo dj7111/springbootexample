d0a42d7433ae99106e9f705f426d33ed2716dfe8
dj7111 <hdouglasjames@hotmail.com>
2018-02-23 16:02:13 -0500

    simple gpio

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+    private static SimpleGPIO simpleGPIO = new SimpleGPIO();
+
 
     @RequestMapping(value = "/blah/{name}", method = RequestMethod.GET)
     public @ResponseBody String blah(  @PathVariable("name") String name) {
@@ -19,6 +21,7 @@ public class SampleController {
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
             produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody String postTest(PostTestData postTestData) {
+        simpleGPIO.toggle();
         return  "{\"post_test\": \"" + postTestData.getStr() + "\"}";
     }
 }
